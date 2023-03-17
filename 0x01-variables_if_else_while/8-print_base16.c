97855474aaaa783b2ca4608b94e759edd4fc7c10
HivdaLasheen <hebasaad91@gmail.com>
2023-03-17 11:39:09 -0700

    8-print_base16.ccommt

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - file name 8-print_base16.c
+  *Return: Always 0.
+  */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'a';
+	while (i <= 'f')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
