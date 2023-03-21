71e0dae592d0e7f34aa8dfe11b5434aa602300b5
HivdaLasheen <hebasaad91@gmail.com>
2023-03-21 07:25:22 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,10 @@
+#include <unistd.h>
 #include"main.h"
 /**
   *main - file name 0-putchar.c
   *Return: Always 0.
   */
 int main(void)
-{
-        _putchar('_')
-        _putchar('p')
-        _putchar('u')
-        _putchar('t')
-        _putchar('c')
-        _putchar('h')
-        _putchar('a')
-        _putchar('r')
-        _putchar('\n');
-        
 {
 	char * = "Holberton";
 
