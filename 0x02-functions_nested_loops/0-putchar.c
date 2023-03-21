1554d97f07c482f8149942053b85ab624b4087ed
HivdaLasheen <hebasaad91@gmail.com>
2023-03-21 05:31:59 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-#include"main.h"
+#include "main.h"
 /**
   *main - file name 0-putchar.c
   *Return: Always 0.
   */
 int main(void)
 {
-	_putchar('_')
-	_putchar('p')
-	_putchar('u')
-	_putchar('t')
-	_putchar('c')
-	_putchar('h')
-	_putchar('a')
-	_putchar('r')
+	char *sh = "0. _putchar";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
+
 	return (0);
 }
