a9b34d63cf80113db6c8d3f557f247e97c280bb0
HivdaLasheen <hebasaad91@gmail.com>
2023-03-22 00:11:05 -0700

    betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-  *main - Entry point
-  *Description: print the alphabet
-  *Return: Always 0
-  */
+  * main - Entry point
+  *
+  * Return: Always (0)
+*/
 void print_alphabet(void)
 {
 	int i;
-	for (i =97; i <= 122; i++)
+
+	for (i = 97; i <= 122; i++)
 	{
 		_putchar(i);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
