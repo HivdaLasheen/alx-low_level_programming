24cbc2b1a33b868cfa5ed987c818ef6c84436efe
HivdaLasheen <hebasaad91@gmail.com>
2023-03-21 23:42:24 -0700

    alph

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
 #include "main.h"
-
 /**
- * print_alphabet -
- *
- */
-void print_alphabet(void)
+  *main - Entry point
+  *Description: print the alphabet
+  *Return: Always 0
+  */
+int main(void)
+	void print_alphabet(void)
 {
 	char ch;
 
@@ -15,4 +17,5 @@ void print_alphabet(void)
 		ch++;
 	}
 	_putchar('\n');
-}
+{
+
