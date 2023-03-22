cba455328d54adfddc0eb81943d5e57900fe2cae
HivdaLasheen <hebasaad91@gmail.com>
2023-03-22 01:53:39 -0700

    comm

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-  * print_alphabet_x10 - Prints alphabet x10
-  * Return : Always o
+  * print_alphabet - Prints alphabet x10
+  * Return : Always 0
   */
 void print_alphabet_x10(void);
 {
