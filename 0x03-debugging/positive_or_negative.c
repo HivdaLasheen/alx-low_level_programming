67e35c79f73c9618e2b48a25116f14d3239dcf92
HivdaLasheen <hebasaad91@gmail.com>
2023-03-23 00:43:10 -0700

    comt

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,12 +3,14 @@
   * main - test function for positive or negative.
   * Return : 0
   */
-int main(void)
+void positive_or_negative(int i)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 
 	return (0);
 }
