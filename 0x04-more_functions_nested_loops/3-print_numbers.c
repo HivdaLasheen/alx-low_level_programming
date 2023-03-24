9f0c6491fe62cf58f3a17c6b46f2798c3fe4683b
HivdaLasheen <hebasaad91@gmail.com>
2023-03-24 11:25:07 -0700

    xx

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-	char c;
+	char numbers;
 
-	for (c = '0'; c <= '9'; c++)
+	for (numbers = '0'; numbers <= '9'; numbers++)
 	{		
-		_putchar(c + '0');
+		_putchar(numbers + '0');
 	}
 	_putchar('\n');
 }
