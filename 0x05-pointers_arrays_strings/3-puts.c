b0f6e9c2388cc4a1153ef7324f6d6d867180f1dd
HivdaLasheen <hebasaad91@gmail.com>
2023-03-28 00:05:21 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints string.
+ * @str: pointer to string.
+ *
+ *Return: void.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
