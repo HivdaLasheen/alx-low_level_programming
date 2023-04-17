6b78c5c89b9c6b6ddaf319fbf04df9e72cff67b8
HivdaLasheen <hebasaad91@gmail.com>
2023-04-17 13:27:41 -0700

    up

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,6 +1,7 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -8,9 +9,9 @@
  */
 int main(void)
 {
-	int s;
+    int s;
 
-	s = 98 + SIZE;
-	printf("%d\n", s);
-	return (0);
+    s = 98 + SIZE;
+    printf("%d\n", s);
+    return (0);
 }
