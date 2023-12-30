a2977f1a1e05c77df8709fcf5fccd8a78088f831
HivdaLasheen <hivdahollz@gmail.com>
2023-12-30 09:41:51 -0800

    gcc error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
-
-int main() {
+//main : 4-puts.c function
+//return: 0
+int main(){
     // Using puts to print the required string
     puts("Programming is like building a multilingual puzzle");
-
     // Return 0 to end the program
     return 0;
 }
