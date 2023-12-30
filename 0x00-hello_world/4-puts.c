207bfc0771a3eec3a514f1f9b62aadd923533bc3
HivdaLasheen <hivdahollz@gmail.com>
2023-12-30 09:51:19 -0800

    commit 4puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+//Return : 0
+//main: 4-puts function
+int main(){
+    // Using puts to print the required string
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to end the program
+    return 0;
+}
