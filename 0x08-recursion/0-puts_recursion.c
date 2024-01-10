26ab221fa045d8bbfa0359e59595d11f23532206
HivdaLasheen <hivdahollz@gmail.com>
2024-01-10 13:33:18 -0800

    komm

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,11 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
-	/** Print a new line when the end of the string is reached */
-	return;
+		/** Print a new line when the end of the string is reached */
+		return;
+	}
 	_putchar(*s);
 	/** Print the current character */
 	_puts_recursion(s + 1);
