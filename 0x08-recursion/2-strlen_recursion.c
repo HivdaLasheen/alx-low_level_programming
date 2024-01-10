ef19627ade649b6c32de6af381d7f272fe92a55c
HivdaLasheen <hivdahollz@gmail.com>
2024-01-10 14:38:25 -0800

    add comment

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,8 +8,11 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
+		/**current char is the null return 0*/
 	{
 		return (0);
 	}
 		return (1 + _strlen_recursion(s + 1));
+		/**recursive call,pointer incremented by one */
+		/** +1 to result of recursive call, returns the sum */
 }
