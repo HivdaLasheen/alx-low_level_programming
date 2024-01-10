972d5d2b6506b4a6d2eed148e608675b2713333b
HivdaLasheen <hivdahollz@gmail.com>
2024-01-10 13:01:55 -0800

    kommit2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,9 @@ void _print_rev_recursion(char *s)
 {
     if (*s == '\0')
     {
-        return;  // Base case: stop recursion when the end of the string is reached
+        return;  /** Base case: stop recursion when the end of the string is reached*/
     }
 
-    _print_rev_recursion(s + 1);  // Recursively call the function with the next character
-    _putchar(*s);  // Print the current character
+    _print_rev_recursion(s + 1);  /** Recursively call the function with the next character */
+    _putchar(*s);  /** Print the current character */
 }
