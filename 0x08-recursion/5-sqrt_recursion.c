6bc52ebf19370aca7909d3e37d7c01816e2241f4
HivdaLasheen <hivdahollz@gmail.com>
2024-01-11 09:02:56 -0800

    Square stuck root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,27 +9,29 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		/** If n is negative, return -1 as it doesn't have a natural square root */
+		/** If n is negative, return -1 */
 		return(-1);
 	}
 	else
 	{
-		return s_root(n,1);
+		return (s_root(n,1));
 	}
+}
 int s_root(int n, int v)
 {
-        /** is exactly n, we found the square root */
-        if (v * v == n)
-        {
-                return(v);
-        }
-        /** less than n */
-        else if (v * v < n)
-        {
-                return(n, v/2);
-        }
-	/**greater than n, returns -1 */
-        else 
-		return(-1);
-}       
-}
+	/** is exactly n, we found the square root */
+	if ( v * v == n)
+	{
+		return(v);
+	}
+	/** less than n */
+	else if (v * v < n)
+	{
+		return(s_root( n, v + 1);										}
+				/** greater than n */
+		else
+		{
+		return (-1);
+		}	
+		}
+		
