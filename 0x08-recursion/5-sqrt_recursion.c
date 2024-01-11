6d2defb85421c3ed56d59c2d915a39759aa2ca3f
HivdaLasheen <hivdahollz@gmail.com>
2024-01-11 08:34:25 -0800

    betty is bitty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,35 @@
 #include "main.h"
 /**
- * _sqrt_recursion : returns natural square.
+ * _sqrt_recursion : function return natural square @n.
  * @n: has no natural square root.
- * Return: -1
+ * Return: negative 1 
  */
+int N_square_root(int n, int value);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		/** If n is negative, return -1 as it doesn't have a natural square root */
-		return (-1);
+		return(-1);
 	}
-	else 
+	else
 	{
-	return _sqrt_recursion (n, 0, n);
+		return N_square_root(n,1);
 	}
+int N_square_root(int n, int value)
+{
+        /** is exactly n, we found the square root */
+        if (value * value == n)
+        {
+                return(value);
+        }
+        /** less than n */
+        else if (value * value < n)
+        {
+                return(n, value/2);
+        }
+	/**greater than n, returns -1 */
+        else 
+		return(-1);
+}       
 }
