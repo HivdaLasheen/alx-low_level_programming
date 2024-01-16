5df0120720ab840731dec31cfbb6eb7d12025c2b
HivdaLasheen <hivdahollz@gmail.com>
2024-01-16 10:52:45 -0800

    malloc commit Task0 betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,11 +3,12 @@
 #include "main.h"
 /**
  * create_array - creates an array of chars,
- *  initializes it with a specific char.
- * @size: input size.
- * @c: input char.
- * return : pointer toarray or NULL if it fails.
-*/
+ * and initializes it with a specific char
+ * @size: input size
+ * @c: input character
+ *
+ * Return: a pointer to the array, or NULL if it fails
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *array;
