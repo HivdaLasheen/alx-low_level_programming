a9fcb49f110de5a81a3a65a2e0987557a91bc582
HivdaLasheen <hivdahollz@gmail.com>
2024-01-17 13:21:24 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,26 +1,21 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * main - prints file name
- * @argc: first parameter
- *
- * @argv: second parameter
+ * @argc: first parameter.
+ * @argv: second parameter.
  * program that prints its name
- * 
  * Return: Always(0) Success
- */
+*/
 int main(int argc, char *argv[])
 {
-    if (argc > 0)
-    {
-       
-        for (int i = 0; argv[0][i] != '\0'; i++)
-        {
-            putchar(argv[0][i]);
-        }
+	int i = 0;
 
-        
-        putchar('\n');
-    }
-
-    return (0);
+	while (argc--)
+	{
+		putchar(argv[0][i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
