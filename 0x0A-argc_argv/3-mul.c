b59f658938295c396769f7acc44c4be1136bd011
HivdaLasheen <hivdahollz@gmail.com>
2024-01-17 15:03:14 -0800

    3-mul.c -o mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,19 +5,21 @@
  *@argc: argument count.
  *@argv: argument array.
  *
- * Return: (0) success, (1) error.
+ * Return: (0)for success, (1)for  error.
 */
 int main(int argc, char *argv[])
 {
+	int num1, num2, result;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-	int result = num1 * num2;
+	result = num1 * num2;
 
 	printf("%d\n", result);
 	return (0);
