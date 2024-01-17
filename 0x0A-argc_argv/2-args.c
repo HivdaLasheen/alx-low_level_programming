38a0e81de8d652d2abd53f6a50b27ce72eb208b6
HivdaLasheen <hivdahollz@gmail.com>
2024-01-17 14:34:54 -0800

    argc2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print all arguments
+ * main - print all arguments it receives
  * Description : print all arguments received.
  * @argc : cmd Line arg count.
  * @argv : smc Line arg arr.
- * Return : Always 0 (Success).
+ *
+ * Return: always(0) (Success).
 */
 int main(int argc, char *argv[])
 {
 	int s;
 
-	for (int i = 0; i < argc; i++)
+	for (int s = 0; s < argc; s++)
 	{
-		printf("%s\n", argv[i]);
+		printf("%s\n", argv[s]);
 	}
 	return (0);
 }
