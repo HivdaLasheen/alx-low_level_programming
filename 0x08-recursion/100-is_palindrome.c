d311cb2a3ee6d1ea7da36466ba49ba37eca6af36
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 10:26:27 +0000

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,11 +9,11 @@
  */
 int check_palindrome(char *str, int length, int index)
 {
-    if (index < length && str[index] == str[length])
-        return (check_palindrome(str, length - 1, index + 1));
-    if (str[index] != str[length])
-        return (0);
-    return (1);
+	if (index < length && str[index] == str[length])
+		return (check_palindrome(str, length - 1, index + 1));
+	if (str[index] != str[length])
+		return (0);
+	return (1);
 }
 
 /**
@@ -23,9 +23,9 @@ int check_palindrome(char *str, int length, int index)
  */
 int get_string_length(char *str)
 {
-    if (*str != '\0')
-        return (1 + get_string_length(str + 1));
-    return (0);
+	if (*str != '\0')
+		return (1 + get_string_length(str + 1));
+	return (0);
 }
 
 /**
@@ -35,8 +35,8 @@ int get_string_length(char *str)
  */
 int is_palindrome(char *str)
 {
-    int index = 0;
-    int length = get_string_length(str) - 1;
+	int index = 0;
+	int length = get_string_length(str) - 1;
 
-    return (check_palindrome(str, length, index));
+	return (check_palindrome(str, length, index));
 }
