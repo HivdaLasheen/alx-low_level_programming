3ccfaecfe05f8c485ceb5c395261655c2b4a0f69
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-06-19 07:41:03 +0000

    commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,23 @@
 #include "main.h"
-#define MY_NULL 0
+#define NULL 0
 
 /**
- * find_first_occurrence - Locate the first occurrence of a character in a string and return a pointer to it.
- * @str: The string to search.
- * @target_char: The target character to find.
- * Return: Pointer to the first occurrence of `target_char` in `str`, or NULL if not found.
- */
-char *find_first_occurrence(char *str, char target_char)
+* _strchr - locate 1st occurrence of char in string and returns pointer there
+* @s: string to search
+* @c: target characer
+* Return: pointer to that character in string
+*/
+
+char *_strchr(char *s, char c)
 {
-    int index = 0;
+	int i = 0;
 
-    while (str[index] != '\0' && str[index] != target_char)
-        index++;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
 
-    if (str[index] == target_char)
-        return (&str[index]);
-    else
-        return (MY_NULL);
+	if (s[i] == c)
+		return (&s[i]);
+
+	else
+		return (NULL);
 }
