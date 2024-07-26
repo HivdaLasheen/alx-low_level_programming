bc1f6a0d045300b4bb9e8c8536a481543a034724
HivdaLasheen <125532618+HivdaLasheen@users.noreply.github.com>
2024-07-26 05:50:21 +0000

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,35 @@
+#ifndef DOG_H
+#define DOG_H
+
 #include <stdio.h>
-#include "dog.h"
+#include <stdlib.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+struct dog
 {
-  struct dog my_dog;
+char *name;
+float age;
+char *owner;
+};
 
-  my_dog.name = "Django";
-  my_dog.age = 3.5;
-  my_dog.owner = "Jay";
-  printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-  return (0);
-}
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+#endif
